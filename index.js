07800ad845e49ed020948f170cebf7f7c16776ce
developer-murat <murat_demir1985@hotmail.com>
2024-01-13 20:04:33 +0300

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+//  Template Literals (şablon dizileri)
+
+const isim = "Mehmet";
+
+const mesaj = `Merhaba, ${isim}`;
+
+console.log(mesaj);
+
+const birim_fiyat = 100;
+
+const adet = 3;
+
+const para_birimi = "TL";
+
+const toplam_tutar = `Toplam: ${birim_fiyat * adet} ${para_birimi}`;
+
+console.log(toplam_tutar);
